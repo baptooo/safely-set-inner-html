7d00276ceba37376a8a7e4e9f6aec4f13747bfc2
Baptiste Rios Campo <baptiste.rios-campo@ekino.com>
2017-12-08 11:33:43 +0100

    refactor(config): change the way of configuring the lib
    - configuration is now done at instanciation

--- src/safelySetInnerHtml.test.js
+++ src/safelySetInnerHtml.test.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import safelySetInnerHtml, { config } from './safelySetInnerHtml';
-
-const defaultConfig = { ...config };
+import SafelySetInnerHtml from './safelySetInnerHtml';
 
 describe('safelySetInnerHtml', () => {
-  beforeEach(() => {
-    Object.assign(config, defaultConfig);
-  });
-
   it('renders correctly', () => {
-    const dom = safelySetInnerHtml('Hello <strong>World !</strong>');
+    const instance = new SafelySetInnerHtml();
+
+    const dom = instance.transform('Hello <strong>World !</strong>');
     const tree = renderer.create(<p>{dom}</p>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it('renders only allowedTags', () => {
-    config.ALLOWED_TAGS = [];
+    const instance = new SafelySetInnerHtml({ ALLOWED_TAGS: [] });
 
-    const dom = safelySetInnerHtml('Hello <strong>World !</strong>');
+    const dom = instance.transform('Hello <strong>World !</strong>');
     const tree = renderer.create(<p>{dom}</p>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it('renders correctly', () => {
-    const dom = safelySetInnerHtml('About the <a href="http://example.com">Author !</a>');
+    const instance = new SafelySetInnerHtml();
+
+    const dom = instance.transform('About the <a href="http://example.com">Author !</a>');
     const tree = renderer.create(<cite>{dom}</cite>).toJSON();
 
     expect(tree).toMatchSnapshot();
