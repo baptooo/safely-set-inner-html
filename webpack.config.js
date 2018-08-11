8afac44eb3061957a6ac7b5e7d9b7bc4568a8c48
Baptiste Rios Campo <baptiste.rioscampo@frichtri.co>
2018-08-11 10:45:29 +0200

    chore(upgrades): webpack4, jest and externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,25 @@
 const path = require('path');
-const webpack = require('webpack');
 
-const isProd = process.env.NODE_ENV === 'production';
-
-let rules = [
-  {
-    test: /\.(js)$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader',
-  }
-];
-let plugins = [];
-
-if (isProd) {
-  plugins = plugins.concat([
-    new webpack.optimize.UglifyJsPlugin()
-  ])
-}
-
-module.exports = {
+module.exports = (env, argv) => ({
   entry: {
     safelySetInnerHTML: path.join(__dirname, 'src/safelySetInnerHTML.js')
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: `[name]${isProd ? '.min' : ''}.js`,
+    filename: `[name]${argv.mode === 'production' ? '.min' : ''}.js`,
     library: 'safelySetInnerHTML',
     libraryTarget: 'umd',
   },
   module: {
-    rules
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
   },
-  plugins,
   externals: {
     react: 'react',
     himalaya: 'himalaya'
   }
-};
+});
